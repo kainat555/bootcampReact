940d7d8e169b7184ba0a280a89e0e7b42188b07e
Kainat <faryashah555@gmail.com>
2020-08-08 09:20:28 -0700

    update with changes in app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,50 +1,47 @@
 import React, { useState } from 'react';
 import './App.css';
-import Info from './info.js';
-import Options from "./options.js";
-import Tech from "./tech.js";
-import Test from "./test.js";
+// import Info from './info.js';
+// import Options from "./options.js";
+// import Tech from "./tech.js";
+// import Test from "./test.js";
 
 
+// function App() {
+//   return (
+//     <div className="main">
+//       <h1>Hello World from me</h1>
+//       <Info name="Ali" age="30" profession="lecturer"/>
+//       <Info name="Aliyan" age="20" profession="Web developer"/> 
+
+//       <h3>Following career options are below:</h3>
+//       <Options firstOption="Medical"/>
+//       <Options secondOption="Engineering"/>
+//       <Options thirdOption="Computer"/>
+
+//       <h3>Front-end technologies are:</h3>
+//       <Tech tech="HTML"/>  
+//       <Tech tech="CSS"/>      
+//       <Tech tech="javascript"/>      
+//       <Tech tech="react"/> 
+
+//       <h3>Back-end technologies are:</h3> 
+//       <Tech tech="node.js"/>
+//       <Tech tech="php"/>
+//       <Tech tech="django"/>    
 
 
+//       <h4>Here I'm for learning React</h4>
+//       <Test fun="It's fun to learn react from PIAIC teachers"/>
+//       <Test option="Yes"/> 
+//       <Test option="No"/>
 
-function App() {
-  return (
-    <div className="main">
-      <h1>Hello World from me</h1>
-      <Info name="Ali" age="30" profession="lecturer"/>
-      <Info name="Aliyan" age="20" profession="Web developer"/> 
+//     </div>
 
-      <h3>Following career options are below:</h3>
-      <Options firstOption="Medical"/>
-      <Options secondOption="Engineering"/>
-      <Options thirdOption="Computer"/>
-
-      <h3>Front-end technologies are:</h3>
-      <Tech tech="HTML"/>  
-      <Tech tech="CSS"/>      
-      <Tech tech="javascript"/>      
-      <Tech tech="react"/> 
-
-      <h3>Back-end technologies are:</h3> 
-      <Tech tech="node.js"/>
-      <Tech tech="php"/>
-      <Tech tech="django"/>    
+//   );
+// }
 
 
-      <h4>Here I'm for learning React</h4>
-      <Test fun="It's fun to learn react from PIAIC teachers"/>
-      <Test option="Yes"/> 
-      <Test option="No"/>
-
-    </div>
-
-  );
-}
-
-
-export default App;
+// export default App;
 
 // export default function App(){
 //  let[count,setCount] = useState(0)
@@ -66,3 +63,48 @@ export default App;
 
 // )
 // }
+
+
+// export default function App(){
+//   let[isMorn,setMorn] = useState(true)
+//   return ( 
+//     <div className="change">
+//       <h2>{isMorn ? "Morning" : "Night"}</h2>
+//       <button onClick={
+//         () => setMorn(!isMorn)
+//       }>
+//         update day
+//       </button>
+//     </div>
+//   )
+// }
+
+
+// export default function App(){
+//   let[isNight , setNight] = useState(true)
+//   return (
+//     <div>
+//       <h1>{isNight ? "Good Night" : "Good Day"}</h1>
+//       <button onClick={
+//         () => setNight(!isNight)
+//       }>
+//         Change
+//       </button>
+//     </div>
+//   )
+// }
+
+
+export default function App(){
+  let[isMorn,setMorn] = useState(true)
+  return(
+    <div className={`${isMorn ? "dayLight" : "nightLight"}`}>
+      <h1>Day Time = {isMorn ? "Morning" : "Night"}</h1>
+      <button onClick={
+        () => setMorn(!isMorn)
+      }>
+        update
+      </button>
+    </div>
+  )
+}
